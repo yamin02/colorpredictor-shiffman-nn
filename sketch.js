775cb1564d18df65a2ff38749466f5e6d0947065
Yaminul Hoque <yamin136635@gmail.com>
2023-05-27 13:41:29 +0600

    project done Trained automatically with an algo

--- sketch.js
+++ sketch.js
@@ -10,16 +10,18 @@ function pickcolor(){
 
  function setup(){
     createCanvas(600,300);
+    // noLoop();
     brain = new NeuralNetwork(3,3,2);
+    automateTraining() ; 
+
     pickcolor()
 }
 
 function colorPredictor(r,g,b){
-
-    // Normalize the data for first layer
-    let inputs = [r/255 , g/225 , b /255];  
-    let outputs = brain.predict(inputs);
-    console.log(outputs);
+    //  console.log(r+g+b)
+    let inputs = [r/255 , g/225 , b /255];      // Normalize the data for first layer to make it between 0 to 1 
+    let outputs = brain.predict(inputs);  // this predicts the the output  from input (r,g,b)
+    // console.log(outputs);
 
     if(outputs[0] > outputs[1]){
         return "black"
@@ -36,7 +38,35 @@ function colorPredictor(r,g,b){
 }
 
 function mousePressed(){
-    // We click on the black or white side of choice 
+    // manualTraing()
+    pickcolor()
+}
+
+
+//Lets automate the training 
+function automateTraining(){
+    for(let i = 0 ; i<100000 ; i++){
+        let ro = random(255) //this r , g , b values are diff from the one set globally
+        let go = random(255)
+        let bo = random(255)
+        let targets = trainColor(ro,go,bo);
+        let inputs = [ro/255 , go/255 , bo/255];
+        brain.train(inputs , targets);
+    }
+}
+
+//Using the same r+g+b < 200 algorithm as training algorithm 
+function trainColor(r,g,b){
+    if(r+g+b > 350 ){
+        return [1,0]   //black
+    }else{
+        return [0,1]   // white
+    }
+}
+
+//manual color picking is tiresome 
+function manualTraing(){
+     // We click on the black or white side of choice 
     // here we are doing a supervised learning
     let targets ;
     if(mouseX > width/2){
@@ -47,15 +77,12 @@ function mousePressed(){
     let inputs = [r/255 , g/255 , b/255];
     brain.train(inputs,targets)   //tain this inputs with this targets
 
-    pickcolor()
 }
-
-
-//manual color picking is tiresome 
 
 
 function draw(){
     background(r,g,b);
+
     strokeWeight(4);
     stroke(0);
     line(width/2 ,0, width/2 , height);
